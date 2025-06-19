b672d9bd370ca347e2960e5c9119ff5edb0709d4
Keisuke Oshima <koisland@sarlacc.pmacs.upenn.edu>
2025-06-19 10:37:08 -0400

    Refactor so utility fns are separate. Fix typos. Ignore logger init error if already init.

--- py/src/lib.rs
+++ py/src/lib.rs
@@ -1,14 +1,13 @@
-use coitrees::{COITree, Interval, IntervalTree};
+use coitrees::{COITree, Interval};
 use core::str;
 use pyo3::{exceptions::PyValueError, prelude::*};
 use pyo3_polars::PyDataFrame;
 use rayon::{prelude::*, ThreadPoolBuilder};
-use rs_nucflag::{
-    io::{read_bed, read_cfg},
-    nucflag,
-    pileup::AlignmentFile,
-};
+use rs_nucflag::{io::read_cfg, nucflag};
 use std::collections::HashMap;
+mod utils;
+
+use crate::utils::{get_aln_intervals, get_ignored_intervals};
 
 #[pyclass]
 pub struct PyNucFlagResult {
@@ -27,89 +26,6 @@ pub struct PyNucFlagResult {
     /// Regions and their status.
     #[pyo3(get)]
     pub regions: PyDataFrame,
-}
-
-pub(crate) fn get_whole_genome_intervals(
-    aln: &str,
-    fasta: Option<&str>,
-    window: usize,
-) -> Result<Vec<Interval<String>>, PyErr> {
-    // If no intervals, apply to whole genome based on header.
-    let mut aln =
-        AlignmentFile::new(aln, fasta).map_err(|err| PyValueError::new_err(err.to_string()))?;
-    let header = aln
-        .header()
-        .map_err(|err| PyValueError::new_err(err.to_string()))?;
-    Ok(header
-        .reference_sequences()
-        .into_iter()
-        .flat_map(|(ctg, ref_seq)| {
-            let ctg_name: String = ctg.clone().try_into().unwrap();
-            let length: usize = ref_seq.length().get();
-            let (num, rem) = (length / window, length % window);
-            let final_start = num * window;
-            let final_itv = Interval::new(
-                final_start as i32,
-                (final_start + rem) as i32,
-                ctg_name.clone(),
-            );
-            (1..num + 1)
-                .map(move |i| {
-                    Interval::new(
-                        ((i - 1) * window) as i32,
-                        (i * window) as i32,
-                        ctg_name.clone(),
-                    )
-                })
-                .chain([final_itv])
-        })
-        .collect())
-}
-
-pub(crate) fn get_aln_intervals(
-    aln: &str,
-    fasta: Option<&str>,
-    bed: Option<&str>,
-    bp_wg_window: usize,
-) -> Result<Vec<Interval<String>>, PyErr> {
-    if let Some(bed) = bed {
-        Ok(read_bed(bed, |name, st, end, _| {
-            Interval::new(st as i32, end as i32, name.to_owned())
-        })
-        .ok_or_else(|| PyValueError::new_err(format!("Unable to read intervals from {bed}")))?)
-    } else {
-        Ok(get_whole_genome_intervals(aln, fasta, bp_wg_window)
-            .map_err(|err| PyValueError::new_err(err.to_string()))?)
-    }
-}
-
-pub(crate) fn get_ignored_intervals(
-    ignore_bed: Option<&str>,
-) -> Result<HashMap<String, COITree<String, usize>>, PyErr> {
-    if let Some(intervals) = ignore_bed.and_then(|bed| {
-        read_bed(bed, |name, start, stop, _| {
-            Interval::new(start as i32, stop as i32, name.to_owned())
-        })
-    }) {
-        Ok(intervals
-            .into_iter()
-            .fold(
-                HashMap::new(),
-                |mut acc: HashMap<String, Vec<Interval<String>>>, x| {
-                    if acc.contains_key(&x.metadata) {
-                        acc.get_mut(&x.metadata).unwrap().push(x);
-                    } else {
-                        acc.entry(x.metadata.clone()).or_insert(vec![x]);
-                    }
-                    acc
-                },
-            )
-            .into_iter()
-            .map(|(rgn, itvs)| (rgn, COITree::new(&itvs)))
-            .collect())
-    } else {
-        Ok(HashMap::default())
-    }
 }
 
 /// Get interval regions from an alignment file or bed file.
@@ -142,7 +58,7 @@ fn run_nucflag_itv(
     let cfg = read_cfg(cfg, preset).map_err(|err| PyValueError::new_err(err.to_string()))?;
     let itv = Interval::new(itv.0, itv.1, itv.2);
     if cfg.general.verbose {
-        simple_logger::init_with_level(log::Level::Debug).expect("Cannot initialize logger.");
+        let _ = simple_logger::init_with_level(log::Level::Debug);
     }
 
     // Set rayon threadpool
@@ -166,6 +82,7 @@ fn run_nucflag_itv(
         .map_err(|err| PyValueError::new_err(err.to_string()))
 }
 
+/// Print config from preset.
 #[pyfunction]
 #[pyo3(signature = (preset = None, cfg = None))]
 fn print_config_from_preset(preset: Option<&str>, cfg: Option<&str>) -> PyResult<()> {
@@ -197,7 +114,7 @@ fn print_config_from_preset(preset: Option<&str>, cfg: Option<&str>) -> PyResult
 ///
 /// # Returns
 /// * A [`PyNucFlagResult`] object where:
-///     * `cov` is a pileup dataframe
+///     * `pileup` is a pileup dataframe
 ///     * `regions` contains all regions evaluated.
 #[pyfunction]
 #[pyo3(signature = (aln, fasta = None, bed = None, ignore_bed = None, threads = 1, cfg = None, preset = None))]
